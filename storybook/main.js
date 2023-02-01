1cfff1ffafb44d9e24bb65c6f5ef0c682ce552ca
JM <proke03@naver.com>
2023-02-02 02:31:51 +0900

    Fix build error

--- storybook/main.js
+++ storybook/main.js
@@ -1,7 +1,11 @@
-const path = require('path');
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
